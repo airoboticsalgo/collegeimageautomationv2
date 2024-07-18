4fbd7fc26370edc7b3941f640408e6074a48d79d
airoboticsalgo <airoboticsalgo@gmail.com>
2024-07-18 19:10:18 +0530

    main fn

--- collegeimageautomation.py
+++ collegeimageautomation.py
@@ -1,4 +1,12 @@
 
 from waitress import serve
 from lib.api import app
-serve(app.app, host='0.0.0.0', port=7055)
+
+def main(): 
+    serve(app.app, host='0.0.0.0', port=7055)
+  
+  
+# Using the special variable  
+# __name__ 
+if __name__=="__main__": 
+    main() 
